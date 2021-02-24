0c597d1f02513e4dad779f63305fd79addcd3e5e
YLin-Z <1615200012@e.gzhu.edu.cn>
2021-02-24 22:31:58 +0800

    to excel

--- Include/main.py
+++ Include/main.py
@@ -4,10 +4,14 @@ import os
 def updaterow(row,numofcol):
     i=1
     print('-----------------------------------------------------------------')
-    print(type(row))
-    while i<numofcol-1:
-        print(row[1][i])
+    # print(type(row))
+    while i<numofcol:
+        # print(row[1][i])
+        if row[1][i] =='-':
+            # print('-')
+            row[1][i]=0
         i=i+1
+    return row
 
 
 
@@ -16,13 +20,17 @@ def updaterow(row,numofcol):
 def updatedata(path):
     df = pd.read_excel(path, sheet_name=0)
     numofcol,numofrow=df.shape
+    result=()
     for row in df.iteritems():
-         updaterow(row,numofcol)  # 每一列填充空位
-
+         newrow = updaterow(row,numofcol)  # 每一列填充空位
+         result =result+newrow
+    print(result)
+    df_result = pd.DataFrame(result)
+    df_result.to_excel("D:/test.xls",index=False)
 
 
 
 
 if __name__ == '__main__':
-    path = "D:/CXCDATA/yibanyuanchuanbiao.xls"#待录入文件的路径
+    path = "D:/CXCDATA/yibanyuanchuanbiaover0.xls"#待录入文件的路径
     updatedata(path)
